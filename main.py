5041e0cdabb8057a90b77afa3c71b9914c93fadd
Befezdow <befezd@gmail.com>
2020-04-09 23:48:42 +0300

    commit for pr

---
+++ main.py
@@ -0,0 +1,77 @@
+from datetime import datetime
+import numpy as np
+
+from torch.utils.tensorboard import SummaryWriter
+
+from preparations import load_and_prepare_data, make_augmentation, normalize_and_vectorize, split_data
+from random_forest import RFClassifier
+from torch_net import TorchNetClassifier
+
+epoch_number = 30
+multiplier = 20
+abductions_count = 12
+needed_per_abduction = 1000
+test_set_size = 0.3
+random_state = 3
+
+if __name__ == '__main__':
+    filename = 'N:\\Nova\\half.csv'
+
+    class_column_name = 'class1'
+    excess_column_names = ['class2', 'class3']
+    base_columns_names = ['gender', 'age', class_column_name, *excess_column_names]
+    names_array = [*base_columns_names, *['t{}'.format(i + 1) for i in range(60000)]]
+
+    raw_data = load_and_prepare_data(filename, names_array)
+    current_time = datetime.now().strftime("%d_%m_%Y-%H_%M_%S")
+    models = [
+        {
+            'name': 'Torch neural network',
+            'classifier': TorchNetClassifier(
+                attrs_count=abductions_count * needed_per_abduction + 2, classes_count=9, batch_size=1000
+            ),
+            'writer': SummaryWriter(f'./logs/torch_net_{current_time}'),
+            'batch_size': 1000,
+        },
+        # {
+        #     'name': 'Random forest',
+        #     'classifier': RFClassifier(estimators_number=110, batch_size=1000),
+        #     'writer': SummaryWriter(f'./logs/rf_{current_time}'),
+        #     'batch_size': 1000,
+        # },
+    ]
+
+    for epoch in range(epoch_number):
+        data = make_augmentation(raw_data, multiplier, abductions_count, needed_per_abduction, base_columns_names)
+        data = normalize_and_vectorize(data, class_column_name, excess_column_names)
+        splitted_data = split_data(data, class_column_name, test_set_size, random_state)
+
+        train_x = splitted_data['train_set']['x']
+        train_y = splitted_data['train_set']['y']
+        train_size = splitted_data['train_set']['size']
+
+        test_x = splitted_data['test_set']['x']
+        test_y = splitted_data['test_set']['y']
+
+        print(f'Epoch: {epoch}')
+        for model in models:
+            name = model['name']
+            classifier = model['classifier']
+
+            batches_count = int(np.ceil(train_size / model['batch_size']))
+
+            def lr_logger(x, y): model['writer'].add_scalar('Train/LR', y, x + epoch * batches_count)
+
+            def loss_logger(x, y): model['writer'].add_scalar('Train/Loss', y, x + epoch * batches_count)
+
+            train_error, train_loss = classifier.fit(train_x, train_y, lr_logger, loss_logger)
+            test_error, test_loss = classifier.check(test_x, test_y)
+
+            model['writer'].add_scalar('Train/Error', train_error, epoch)
+            model['writer'].add_scalar('Test/Error', test_error, epoch)
+            model['writer'].add_scalar('Test/Loss', test_loss, epoch)
+
+            print(f'[Name: {name}]; '
+                  f'[Train: error - {train_error}; loss - {train_loss}]; '
+                  f'[Test: error - {test_error}; loss - {test_loss}'
+            )
---
+++ torch_net.py
@@ -0,0 +1,97 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import numpy as np
+
+
+class Net(torch.nn.Module):
+    def __init__(self, attrs_count, classes_count):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(attrs_count, 500)
+        self.fc2 = nn.Linear(500, 500)
+        self.fc3 = nn.Linear(500, classes_count)
+
+    def forward(self, x):
+        x = nn.functional.relu(self.fc1(x))
+        x = nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
+        return nn.functional.log_softmax(x, dim=1)
+
+
+class TorchNetClassifier:
+    def __init__(self, attrs_count, classes_count, batch_size=1000):
+        # Инициализируем нейронную сеть
+        self.model = Net(attrs_count, classes_count).double()
+        # Создаем оптимизатор
+        self.optimizer = torch.optim.Adam(self.model.parameters())
+        # Создаем функцию потерь
+        self.criterion = torch.nn.NLLLoss()
+
+        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=10, gamma=0.1)
+
+        self.batch_size = batch_size
+
+    def fit(self, x_train, y_train, lr_logger=None, loss_logger=None):
+        x_tensor = torch.Tensor(x_train.to_numpy().astype(np.float32))
+        y_tensor = torch.Tensor(y_train.to_numpy()).long() - 1
+
+        dataset_size = x_tensor.shape[0]
+        batch_size = self.batch_size
+        batches_count = int(np.ceil(dataset_size / batch_size))
+
+        correct = 0
+        summary_loss = 0
+        for batch_index in range(batches_count):
+            x_var = Variable(x_tensor[batch_index * batch_size:batch_index * batch_size + batch_size])
+            y_var = Variable(y_tensor[batch_index * batch_size:batch_index * batch_size + batch_size])
+
+            self.optimizer.zero_grad()
+            net_out = self.model(x_var.double())
+            loss = self.criterion(net_out, y_var)
+
+            _, pred = net_out.data.max(1)  # получаем индекс максимального значения
+
+            loss.backward()
+            self.optimizer.step()
+
+            correct += pred.eq(y_var.data).sum()
+            summary_loss += loss.item()
+
+            if lr_logger is not None:
+                lr_value = self.scheduler.get_lr()[0]
+                lr_logger(batch_index, lr_value)
+
+            if loss_logger is not None:
+                loss_value = loss.item()
+                loss_logger(batch_index, loss_value)
+
+        self.scheduler.step()
+        return 1 - correct.item() / dataset_size, summary_loss / dataset_size
+
+    def check(self, x, y):
+        x_tensor = torch.Tensor(x.to_numpy().astype(np.float32))
+        y_tensor = torch.Tensor(y.to_numpy()).long() - 1
+
+        dataset_size = x_tensor.shape[0]
+        batch_size = self.batch_size
+        batches_count = int(np.ceil(dataset_size / batch_size))
+
+        correct = 0
+        summary_loss = 0
+        for batch_index in range(batches_count):
+            x_var = Variable(x_tensor[batch_index * batch_size:batch_index * batch_size + batch_size])
+            y_var = Variable(y_tensor[batch_index * batch_size:batch_index * batch_size + batch_size])
+
+            net_out = self.model(x_var.double())
+            loss = self.criterion(net_out, y_var)
+
+            _, pred = net_out.data.max(1)  # получаем индекс максимального значения
+            correct += pred.eq(y_var.data).sum()
+            summary_loss += loss.item()
+
+        return 1 - correct.item() / dataset_size, summary_loss / dataset_size
+
+    def predict(self, x):
+        x_tensor = torch.Tensor(x.to_numpy())
+        x_var = Variable(x_tensor)
+        return self.model(x_var.double())
